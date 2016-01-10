4ff77c51513e2af2921376c418cd1182c35066e0
abhirooptan <abhirooptan@live.com>
2016-01-10 12:56:05 +0000

    Assets folder Added

---
+++ Assets/Scripts/Camera2DFollow.cs
@@ -0,0 +1,100 @@
+using UnityEngine;
+
+// this comes from the Unity Standard Assets
+namespace UnityStandardAssets._2D
+{
+    public class Camera2DFollow : MonoBehaviour
+    {
+        public Transform target;
+        public float damping = 1;
+        public float lookAheadFactor = 3;
+        public float lookAheadReturnSpeed = 0.5f;
+        public float lookAheadMoveThreshold = 0.1f;
+
+        public float shakeTimer;
+        public float shakeAmount;
+
+        [SerializeField]
+        private float xMin;
+        [SerializeField]
+        private float xMax;
+        [SerializeField]
+        private float yMin;
+        [SerializeField]
+        private float yMax;
+
+		// private variables
+        float m_OffsetZ;
+        Vector3 m_LastTargetPosition;
+        Vector3 m_CurrentVelocity;
+        Vector3 m_LookAheadPos;
+
+        // Use this for initialization
+        private void Start()
+        {
+            m_LastTargetPosition = target.position;
+            m_OffsetZ = (transform.position - target.position).z;
+            transform.parent = null;
+
+			// if target not set, then set it to the player
+			if (target==null) {
+				target = GameObject.FindGameObjectWithTag("Player").transform;
+			}
+
+			if (target==null)
+				Debug.LogError("Target not set on Camera2DFollow.");
+
+        }
+
+        // Update is called once per frame
+		private void Update()
+        {
+			if (target == null)
+				return;
+
+            // only update lookahead pos if accelerating or changed direction
+            float xMoveDelta = (target.position - m_LastTargetPosition).x;
+
+            bool updateLookAheadTarget = Mathf.Abs(xMoveDelta) > lookAheadMoveThreshold;
+
+            if (updateLookAheadTarget)
+            {
+                m_LookAheadPos = lookAheadFactor*Vector3.right*Mathf.Sign(xMoveDelta);
+            }
+            else
+            {
+				m_LookAheadPos = Vector3.MoveTowards(m_LookAheadPos, Vector3.zero, Time.deltaTime*lookAheadReturnSpeed);
+            }
+
+            Vector3 aheadTargetPos = target.position + m_LookAheadPos + Vector3.forward*m_OffsetZ;
+            Vector3 newPos = Vector3.SmoothDamp(transform.position, aheadTargetPos, ref m_CurrentVelocity, damping);
+            
+            //if(target.position.x > xMin && target.position.x < xMax)
+            transform.position = newPos;
+            transform.position = new Vector3(Mathf.Clamp(target.position.x, xMin, xMax), Mathf.Clamp(target.position.y, yMin, yMax), -10);
+
+            m_LastTargetPosition = target.position;
+
+            if (Input.GetKeyDown(KeyCode.Z))
+            {
+                ShakeCamera(0.1f, 0.5f);
+            }
+
+            if (shakeTimer >= 0)
+            {
+                Debug.Log("Trying to Shake Camera");
+                Vector2 shakePos = Random.insideUnitCircle * shakeAmount;
+                transform.position = new Vector3(transform.position.x + shakePos.x, transform.position.y + shakePos.y, transform.position.z);
+
+                shakeTimer -= Time.deltaTime;
+            }
+        }
+
+        public void ShakeCamera(float shakePwr, float shakeDur)
+        {
+            Debug.Log("Inside Shake Camera");
+            shakeAmount = shakePwr;
+            shakeTimer = shakeDur;
+        }
+    }
+}
---
+++ Assets/Scripts/EnemyStun.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemyStun : MonoBehaviour {
+
+    // if Player hits the stun point of the enemy, then call Stunned on the enemy
+    void OnCollisionEnter2D(Collision2D other)
+	{
+		if (other.gameObject.tag == "Player")
+		{
+			// tell the enemy to be stunned
+			this.GetComponentInParent<Enemy>().Stunned();
+            other.gameObject.GetComponent<CharacterController2D>().SlowMotion();
+            //camera.ShakeCamera(0.1f, 0.5f);
+            GameObject.Find("Main Camera").GetComponent<UnityStandardAssets._2D.Camera2DFollow>().ShakeCamera(0.1f,0.1f);
+
+            // make the player bounce of the enemies head
+            other.gameObject.GetComponent<CharacterController2D>().EnemyBounce();
+        }
+	}
+}
